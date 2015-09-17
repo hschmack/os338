75f7abc2ff472949b9431a4e5172dc5ccb8eebed
Hayden Schmackpfeffer <hbs25@case.edu>
2015-09-16 22:36:16 -0700

    alternating processes now, need to add in printout info

--- a1/main.c
+++ a1/main.c
@@ -17,7 +17,8 @@
 #define DELAY 1 //second
 
 //Function Declarations
-static void child(int child_number);
+void child_proc(int bin_start, int child_number);
+void child(int child_number);
 static int bin_coefficient(int n, int r);
 int n;
 
@@ -25,20 +26,32 @@ int main(int argc, char *argv[]) {
   n = 2;
   int i;
   int status = 0;
-  pid_t pid, wpid;
+  pid_t child1_pid, child2_pid, child3_pid, child4_pid;
 
-  for (i =0; i < NUM_CHILDREN; i++){
-    if(i == 2) { sleep(1);}
-    pid = fork();
-    if(pid < 0) {
-        printf("Error");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        printf("Child (%d): %d\n", i + 1, getpid());
-        child(i+1);
-        exit(EXIT_SUCCESS); 
-    } else  {
-        wait(NULL);
+  child1_pid = fork();
+  if (child1_pid == 0) {
+    printf("(%d (%d - 2)) binomial coefficent computations of integers n=2, 3, 10, start now! \n", MAX_N, MAX_N);
+  } else {
+    //these are executing first because it's the parent process. Is this a big deal?
+    child2_pid = fork();
+    if (child2_pid == 0) {
+      sleep(1);
+
+      child_proc(2, 2);
+    }
+
+    child3_pid = fork();
+    if (child3_pid == 0) {
+      sleep(2);
+
+      child_proc(3, 3);
+    }
+
+    child4_pid = fork();
+    if (child4_pid == 0){
+      sleep(12);
+      printf("Child 4 (PID = %d, PPID = %d) executing 'ls -l' \n", getpid(), getppid());
+      system("ls -l");
     }
   }
 /*
@@ -50,7 +63,20 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-static void child(int child_number){
+void child_proc(int bin_start, int child_number){
+  //get start time
+  sleep(2);
+  int i;
+  for (i = bin_start; i < 11; i += 2) {
+    int result = bin_coefficient(i, i-2);
+    printf("CHILD %d printing binary coefficient of %d and %d which is %d \n", child_number, i, i-2, result);
+    sleep(2);
+  }
+  //print current time - start time before exiting
+  exit(EXIT_SUCCESS);
+}
+
+void child(int child_number){
 
   if(child_number == 1){
     printf("(%d (%d - 2)) binomial coefficent computations of integers n=2, 3, 10, start now! \n", MAX_N, MAX_N);
